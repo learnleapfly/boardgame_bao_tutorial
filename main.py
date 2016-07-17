ea2d4fea4e7973fac75e30687cc13f9356d24cdf
Kjell Wooding <kjell@wooding.org>
2016-07-16 23:57:45 -0400

    add a stone_locations property to track the stones

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
+from kivy.properties import ObjectProperty
 import bao_engine as Bao
 from kivy.logger import Logger
 
@@ -10,8 +11,8 @@ class Pit(BoxLayout):
         Logger.debug('Pit: Touch on {}'.format(self.pit_obj))
 
 
-
 class BaoGame(BoxLayout):
+    stone_locations = ObjectProperty(None)
     def __init__(self, **kwargs):
         super(BaoGame, self).__init__(**kwargs)
         self.engine = Bao.Game()
@@ -28,11 +29,15 @@ class BaoGame(BoxLayout):
                     c2.pit_obj = self.engine.pits[c2.pit_id]
                     Logger.debug('Link Pits: linked pit {} to {}'.format(c2.pit_id, c2.pit_obj))
 
+    def on_stone_locations(self, inst, value):
+        '''Update the stones by animating them to their final location'''
+        Logger.debug("Bao: stone_locations: {}".format(value))
 
     def start_game(self):
         '''Do the initial sow (place) to start a game'''
         self.engine.initial_place()
         self.toolbar.start_button.disabled = True
+        self.stone_locations = self.engine.stones
 
 class BaoApp(App):
     def build(self):
